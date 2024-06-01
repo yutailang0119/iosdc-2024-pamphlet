f79bed2cf8d859dbb2ed030fce4f321cee7e6946
yutailang0119 <yutailang0119@gmail.com>
2024-06-02 02:33:56 +0900

    Add ExampleApp

---
+++ ExampleApp/ExampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Yutaro Muta on 2024/06/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExampleApp/ExampleApp/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  ExampleApp
+//
+//  Created by Yutaro Muta on 2024/06/02.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
