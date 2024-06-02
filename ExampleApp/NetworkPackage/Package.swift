3262e5ef813ebe8aa5907a0be6847f22dc9d7f03
yutailang0119 <yutailang0119@gmail.com>
2024-06-02 21:08:40 +0900

    Fix NetworkPackage to cleanup

--- ExampleApp/NetworkPackage/Package.swift
+++ ExampleApp/NetworkPackage/Package.swift
@@ -13,18 +13,12 @@ let package = Package(
         .visionOS(.v1),
     ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "NetworkPackage",
-            targets: ["NetworkPackage"]),
+            name: "NetworkLibrary",
+            targets: ["NetworkLibrary"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkPackage"),
-        .testTarget(
-            name: "NetworkPackageTests",
-            dependencies: ["NetworkPackage"]),
+            name: "NetworkLibrary"),
     ]
 )
