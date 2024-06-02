70372468fb525d520d903d3bc1a6dbe25dacc1c8
yutailang0119 <yutailang0119@gmail.com>
2024-06-02 20:44:41 +0900

    Fix NetworkPackage to set latest platforms

--- ExampleApp/NetworkPackage/Package.swift
+++ ExampleApp/NetworkPackage/Package.swift
@@ -5,6 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "NetworkPackage",
+    platforms: [
+        .macOS(.v14),
+        .iOS(.v17),
+        .tvOS(.v17),
+        .watchOS(.v10),
+        .visionOS(.v1),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
