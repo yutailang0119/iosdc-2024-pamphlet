918d4af7da53826bce71cf805ab0487ef75c92fe
yutailang0119 <yutailang0119@gmail.com>
2024-06-07 00:36:24 +0900

    Add ConnectionData

---
+++ ExampleApp/NetworkPackage/Sources/UDPExample/ConnectionData.swift
@@ -0,0 +1,32 @@
+//
+//  ConnectionData.swift
+//  UDPExample
+//
+//  Created by Yutaro Muta on 2024/06/06.
+//
+
+import Foundation
+
+struct ConnectionData: Equatable, Codable {
+    struct Row: Equatable, Codable {
+        enum Item: Equatable, Codable {
+            case empty
+            case host
+            case challenger
+        }
+
+        var leading: Item
+        var center: Item
+        var trailing: Item
+    }
+
+    var top: Row
+    var center: Row
+    var bottom: Row
+
+    init() {
+        self.top = Row(leading: .empty, center: .empty, trailing: .empty)
+        self.center = Row(leading: .empty, center: .empty, trailing: .empty)
+        self.bottom = Row(leading: .empty, center: .empty, trailing: .empty)
+    }
+}
